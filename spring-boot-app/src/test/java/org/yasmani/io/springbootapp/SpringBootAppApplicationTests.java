8e367dc9032caf98190f19a844637deb876210c6
Yasmani Rosado <tas185@gmail.com>
2026-09-16 10:15:38 -0500

    add project spring boot

---
+++ spring-boot-app/src/test/java/org/yasmani/io/springbootapp/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
